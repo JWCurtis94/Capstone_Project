2a58fe6d66d65dedf7bffe86a35b46b52a0dcfa8
James Curtis <jameswcurtis94@outlook.com>
2024-09-16 15:31:18 +0000

    Added core views in core/views.py. This includes home.html file. Also added paths inside urls.py of main project.

--- w2w_league/w2w_league/urls.py
+++ w2w_league/w2w_league/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from core import views as core_views
+from accounts import views as accounts_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', core_views.home, name='home'),
+    path('register/', accounts_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('logout/', auth_views.LogOutView.as_view(template_name='accounts/logout.html'), name='logout'),
 ]
