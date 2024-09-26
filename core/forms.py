c070d0c573823e55b218d1a2c096bf7e149a929c
James Curtis <jameswcurtis94@outlook.com>
2024-09-26 14:58:54 +0000

    added incidentform into core/forms.py

--- core/forms.py
+++ core/forms.py
@@ -1,7 +1,16 @@
 from django import forms
 from .models import RaceResult
+from .models import IncidentTicket
 
 class RaceResultForm(forms.ModelForm):
     class Meta:
         model = RaceResult
         fields = ['race', 'position', 'fastest_lap']
+
+class IncidentTicketForm(forms.ModelForm):
+    class Meta:
+        model = IncidentTicket
+        fields = ['username', 'email', 'race_name', 'incident_description']
+        widgets = {
+            'incident_description': forms.Textarea(attrs={'rows': 5}),
+        }
