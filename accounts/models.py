0225618f6562ca133f971e13c402afa1ddae825e
James Curtis <jameswcurtis94@outlook.com>
2024-09-27 13:24:51 +0000

    Removed unnessersery models from accounts/models.py

--- accounts/models.py
+++ accounts/models.py
@@ -5,25 +5,18 @@ from PIL import Image
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='profile_pics', null=True, blank=True)  # Remove default image
+    image = models.ImageField(upload_to='profile_pics', null=True, blank=True)  # Optional profile image
     bio = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    # Optional: Override save method to resize image
+    # Override save method to resize image if needed
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
-        img = Image.open(self.image.path)
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
-
-class Verdict(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    date = models.DateField()
-
-    def __str__(self):
-        return self.title
+        if self.image:  # Ensure image exists before trying to resize
+            img = Image.open(self.image.path)
+            if img.height > 300 or img.width > 300:
+                output_size = (300, 300)
+                img.thumbnail(output_size)
+                img.save(self.image.path)
