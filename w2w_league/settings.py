364474d25c0e76b4d03497902488009fa56097f5
James Curtis <jameswcurtis94@outlook.com>
2024-10-02 15:23:59 +0000

    add new colours to update css and make sight text more readable

--- w2w_league/settings.py
+++ w2w_league/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '8000-jwcurtis94-capstoneproj-ms2mjob24gt.ws.codeinstitute-ide.net',
+    '8000-jwcurtis94-capstoneproj-br8jv27hyf4.ws.codeinstitute-ide.net',
     '8000-jwcurtis94-capstoneproj-szixg7vt8q9.ws.codeinstitute-ide.net',
     'w2w-791ab20c5eb9.herokuapp.com',
     '8000-jwcurtis94-capstoneproj-din15cczels.ws.codeinstitute-ide.net',
@@ -46,7 +46,7 @@ ALLOWED_HOSTS = [
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://8000-jwcurtis94-capstoneproj-ms2mjob24gt.ws.codeinstitute-ide.net',
+    'https://8000-jwcurtis94-capstoneproj-br8jv27hyf4.ws.codeinstitute-ide.net',
     'https://8000-jwcurtis94-capstoneproj-fj4am88dcuv.ws.codeinstitute-ide.net',
     'https://w2w-791ab20c5eb9.herokuapp.com',
     'https://8000-jwcurtis94-capstoneproj-oi6nickhqe0.ws.codeinstitute-ide.net',
